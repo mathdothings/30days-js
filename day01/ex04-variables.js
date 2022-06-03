36f377291610b430279b154646e90aa1eb69b188
mathdothings <jhonata.prodrigues@gmail.com>
2022-06-03 17:16:59 -0300

    day01 challenges: done

---
+++ day01/ex04-variables.js
@@ -0,0 +1,26 @@
+/*
+ * variables
+ * strings
+ * boolean
+ * undefined
+ * null
+ */
+
+let variable1, variable2, variable3;
+
+let str1 = "my string";
+let str2 = "my string 2";
+let str3 = "my string 3";
+
+let bol = true;
+let bol2 = false;
+
+let undf = undefined;
+
+let n = null;
+
+console.log(variable1, variable2, variable3);
+console.log(str1, str2, str3);
+console.log(bol, bol2);
+console.log(undf);
+console.log(n);
---
+++ day01/ex05-datatypes.js
@@ -0,0 +1,26 @@
+/*
+ * variables
+ * strings
+ * boolean
+ * undefined
+ * null
+ */
+
+let variable1, variable2, variable3;
+
+let str1 = "my string";
+let str2 = "my string 2";
+let str3 = "my string 3";
+
+let bol = true;
+let bol2 = false;
+
+let undf = undefined;
+
+let n = null;
+
+console.log(typeof variable1, typeof variable2, typeof variable3);
+console.log(typeof str1, typeof str2, typeof str3);
+console.log(typeof bol, typeof bol2);
+console.log(typeof undf);
+console.log(typeof n);
