eb443c854d2fde17a1caeb1ddfd85d897a17da24
mathdothings <jhonata.prodrigues@gmail.com>
2022-06-25 08:33:24 -0300

    day 2: done

--- day02/ex-lv3-04.js
+++ day02/ex-lv3-04.js
@@ -1,5 +1,7 @@
 let sent =
-  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus 15000 euro online courses per month.";
 
 let pattern = /\d+/g;
-console.log(sent.match(pattern));
+const income = sent.match(pattern);
+
+console.log(`Annual Income: ${+income[0] * 12 + +income[1] + +income[2] * 12}`);
