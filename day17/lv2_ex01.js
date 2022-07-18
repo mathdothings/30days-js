392948349115fbe5d90362d641ca20f1cdbe2ab2
mathdothings <jhonata.prodrigues@gmail.com>
2022-07-18 06:44:25 -0300

    day 17: todo

--- day17/lv2_ex01.js
+++ day17/lv2_ex01.js
@@ -4,13 +4,16 @@ const student = {
   firstName: "first",
   lastName: "last",
   age: 100,
+  country: "anywhere",
   skills: ["html", "css", "js"],
   enrolledKeys: ["react", "express", "node"],
 };
 
-const { firstName, lastName, age, skills, enrolledKeys } = student;
-const allProps = [firstName, lastName, age, skills, enrolledKeys];
+const { firstName, lastName, age, country, skills, enrolledKeys } = student;
 
-for (const prop of allProps) {
-  localStorage.setItem(JSON.stringify(prop), JSON.stringify(prop));
-}
+localStorage.setItem("firstName", firstName);
+localStorage.setItem("lastName", lastName);
+localStorage.setItem("age", age);
+localStorage.setItem("country", country);
+localStorage.setItem("skills", skills);
+localStorage.setItem("enrolledKeys", enrolledKeys);
