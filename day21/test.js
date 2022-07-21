345b5710da5b0e162a025848089b3d65d4487f84
mathdothings <jhonata.prodrigues@gmail.com>
2022-07-21 11:33:06 -0300

    day 21: todo

--- day21/test.js
+++ day21/test.js
@@ -1,10 +1,21 @@
+// const allTitles = document.getElementsByTagName("h1");
+// const allTitles = document.getElementsByClassName("title");
+
 const allTitles = document.getElementsByTagName("h1");
 
-// console.log(allTitles.length);
-// console.log(allTitles);
+allTitles[3].className = "title";
+allTitles[3].id = "fourth-title";
+allTitles[3].classList.add("title-ascent");
+allTitles[3].innerText = "My new title";
+allTitles[3].innerText = "This is a new one";
 
-for (const title of allTitles) {
-  console.log(title);
+const firstTitle = document.querySelectorAll("#first-title"); // select id with first-title
+// returns a nodelist -> array-like object
+
+const first = document.querySelector("#first-title");
+
+// console.log(first);
+
+for (const item of allTitles) {
+  console.log(item); // catch all elements of that node list
 }
-
-allTitles.forEach((title) => console.log(title));
