4f12c7f3366c3a5d4515bfa249f246a2bbfab951
mathdothings <jhonata.prodrigues@gmail.com>
2022-07-21 22:08:30 -0300

    day 21: todo

---
+++ day21/lv1_ex01.js
@@ -0,0 +1,5 @@
+const firstP = document.querySelector("p");
+
+console.log(firstP);
+firstP.style.backgroundColor = "gray";
+firstP.style.padding = "1rem";
--- day21/test.js
+++ day21/test.js
@@ -2,12 +2,23 @@
 // const allTitles = document.getElementsByClassName("title");
 
 const allTitles = document.getElementsByTagName("h1");
+const titles = document.querySelectorAll("h1");
 
 allTitles[3].className = "title";
 allTitles[3].id = "fourth-title";
 allTitles[3].classList.add("title-ascent");
 allTitles[3].innerText = "My new title";
 allTitles[3].innerText = "This is a new one";
+allTitles[3].textContent = "textContent";
+
+titles.forEach((title, i) => {
+  if (i % 2 === 0) {
+    title.classList.add("red");
+  } else {
+    title.classList.add("green");
+    title.classList.add("hide");
+  }
+});
 
 const firstTitle = document.querySelectorAll("#first-title"); // select id with first-title
 // returns a nodelist -> array-like object
@@ -19,3 +30,5 @@ const first = document.querySelector("#first-title");
 for (const item of allTitles) {
   console.log(item); // catch all elements of that node list
 }
+
+console.log();
