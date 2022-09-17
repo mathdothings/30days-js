b83fc124526237a471b10e69e609f20c1ea974e2
mathdothings <jhonata.prodrigues@gmail.com>
2022-09-16 23:05:59 -0300

    wip

--- day25/js/script.js
+++ day25/js/script.js
@@ -1,5 +1,16 @@
 const countries = countries_data;
 
 countries.forEach((country) => {
-  console.log(country.name, country.population, country.languages);
+  const ul = document.createElement("ul");
+  document.body.appendChild(ul);
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  ul.appendChild(li);
+  li.appendChild(img);
+  img.src = country.flag;
+  img.style.display = "block";
+  img.style.width = "100px";
+  img.style.height = "100px";
+  img.style.borderRadius = "50%";
+  img.style.margin = "1rem auto";
 });
