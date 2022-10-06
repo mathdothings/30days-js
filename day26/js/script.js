9d7cf75c4f27b8aaa6c5062359268f2b59e5f272
mathdothings <jhonata.prodrigues@gmail.com>
2022-10-06 08:28:06 -0300

    wip

--- day26/js/script.js
+++ day26/js/script.js
@@ -1,1 +1,28 @@
 console.log("Start");
+
+/* 
+  self-invoking function is a way to leave out of global scope important script's functions, without the need of declare another 'scope function'. Functions in global score can be invoked directly from user's browser's console
+*/
+(function main() {
+  const URL = "https://restcountries.com/v3.1/all";
+  const getData = (url) => {
+    let APIDATA = [];
+    fetch(url).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      response.json().then((data) => {
+        APIDATA = data;
+        console.log(
+          APIDATA.filter((item) => {
+            if (item.name.common.startsWith("A")) {
+              return item;
+            }
+          })
+        );
+      });
+    });
+  };
+  getData(URL);
+})();
