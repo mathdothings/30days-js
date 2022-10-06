b33c97887589cfc07cd1a58f244620ba79985a3b
mathdothings <jhonata.prodrigues@gmail.com>
2022-10-06 10:22:08 -0300

    wip

--- day26/js/script.js
+++ day26/js/script.js
@@ -1,19 +1,39 @@
-// disable buttons until data fetch is fullified
-window.onload = () => {
-  console.log("Disabling features");
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.disabled = true;
-  });
-  const input = document.querySelector("input");
-  input.disabled = true;
-};
-
 /* 
   self-invoking function is a way to leave out of global scope important script's functions, without the need of declare another 'scope function'. Functions in global score can be invoked directly from user's browser's console
 */
 (function main() {
+  const disableFeatures = () => {
+    // disable buttons until data fetch is fullified
+    window.onload = () => {
+      console.log("Disabling features...");
+      const buttons = document.querySelectorAll("button");
+      buttons.forEach((button) => {
+        button.disabled = true;
+      });
+      const input = document.querySelector("input");
+      input.disabled = true;
+      const header = document.querySelector("header");
+      header.style.visibility = "hidden";
+      console.log("Features disabled");
+    };
+  };
+
+  const enableFeatures = () => {
+    console.log("Enabling features...");
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => (button.disabled = false));
+    const input = document.querySelector("input");
+    input.disabled = false;
+    input.focus();
+    const header = document.querySelector("header");
+    header.style.visibility = "visible";
+    console.log("Features enabled");
+  };
+
+  disableFeatures();
+
   const URL = "https://restcountries.com/v3.1/all";
+
   const getData = (url) => {
     let APIDATA = [];
     fetch(url).then((response) => {
@@ -25,16 +45,10 @@ window.onload = () => {
         .json()
         .then((data) => {
           APIDATA = data;
-          console.log(APIDATA);
+          console.log("Data fetched sucessfully!");
+          console.log("Data:", APIDATA);
         })
-        .then(() => {
-          console.log("Enabling features");
-          const buttons = document.querySelectorAll("button");
-          buttons.forEach((button) => (button.disabled = false));
-          const input = document.querySelector("input");
-          input.disabled = false;
-          input.focus();
-        });
+        .then(enableFeatures());
     });
   };
 
