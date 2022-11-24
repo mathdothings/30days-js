8ff48c73c939c99c697e01ad5c16e87547775dd3
mathdothings <jhonata.prodrigues@gmail.com>
2022-11-24 08:36:16 -0300

    wip

--- day26/js/script.js
+++ day26/js/script.js
@@ -2,24 +2,22 @@
   self-invoking function is a way to leave out of global scope important script's functions, without the needed of declare another 'scope function'. Functions in global scope can be invoked directly from user's browser's console
 */
 (function main() {
+  const URL = "https://restcountries.com/v3.1/all";
+
+  // disable buttons until data fetch is fullified
   const disableButtons = () => {
-    // disable buttons until data fetch is fullified
-    window.onload = () => {
-      console.log("Disabling features...");
-      const buttons = document.querySelectorAll("button");
-      buttons.forEach((button) => {
-        button.disabled = true;
-      });
-      const input = document.querySelector("input");
-      input.disabled = true;
-      const header = document.querySelector("header");
-      header.style.visibility = "hidden";
-      console.log("Features disabled");
-    };
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+    const input = document.querySelector("input");
+    input.disabled = true;
+    const header = document.querySelector("header");
+    header.style.visibility = "hidden";
   };
 
+  // enable buttons when data fetch is fullified
   const enableButtons = () => {
-    console.log("Enabling features...");
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => (button.disabled = false));
     const input = document.querySelector("input");
@@ -27,47 +25,39 @@
     input.focus();
     const header = document.querySelector("header");
     header.style.visibility = "visible";
-    console.log("Features enabled");
   };
 
-  disableButtons();
-
-  const URL = "https://restcountries.com/v3.1/all";
-
-  const getData = (url) => {
-    let APIDATA = [];
-    fetch(url).then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      response
-        .json()
-        .then((data) => {
-          APIDATA = data;
-          console.log("Data fetched sucessfully!");
-          console.log("Data:", APIDATA);
-        })
-        .then(enableButtons())
-        .then(observeClickedButton());
-    });
+  // fetch and return a json from a specific url
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.warn(error);
+    }
   };
 
+  // returns what button the user is clicking
   const observeClickedButton = () => {
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
       button.onclick = () => {
-        console.log(button.innerHTML);
         return button;
       };
     });
   };
 
+  const start = async () => {
+    disableButtons();
+    await getData(URL);
+    enableButtons();
+    observeClickedButton();
+  };
+
+  start();
+
   // Debug -->
-  setTimeout(() => {
-    return getData(URL);
-  }, 2000);
 
   // <--
-  // getData(URL);
 })();
